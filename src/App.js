338ac6faaf0b8c7240ef2bb0bef929eda9b4cfa7
Abdelaziz <fazouz2010@gmail.com>
2022-05-07 16:59:54 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [page, setPage] = React.useState(1)
+    
+    React.useEffect(function() {
+        console.log("Effect ran")
+        fetch(`https://swapi.dev/api/people/${page}`)
+            .then(res => res.json())
+            .then(data => setStarWarsData(data))
+    }, [page])
+    
+    return (
+        <div>
+            <h2>The page is {page}</h2>
+            <button onClick={() => setPage(prevPage => prevPage + 1)}>Get Next Character</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
+    )
 }
-
-export default App;
